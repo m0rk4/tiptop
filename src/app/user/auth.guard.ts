8c11f1a5d80c15c30269fcdd25a2af34944af6ea
Mark Putyato <mark_putyato@mail.ru>
2022-04-30 16:04:08 +0300

    Added auth guard with login redirect

---
+++ src/app/user/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { SnackService } from '../services/snack.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private snackService: SnackService
+  ) {}
+
+  async canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean> {
+    const user = await this.afAuth.currentUser;
+    const isLoggedIn = !!user;
+    if (!isLoggedIn) {
+      this.snackService.authError();
+    }
+
+    return isLoggedIn;
+  }
+}
